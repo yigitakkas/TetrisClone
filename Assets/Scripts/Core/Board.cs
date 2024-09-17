17c7599198bf35a51b1cbf101157ec174ab005d0
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2024-09-17 19:27:15 +0300

    Update board grid creator script

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Transform EmptySprite;
+    public int Height = 30;
+    public int Width = 10;
+    public int Header = 8;
+
+    Transform[,] _grid;
+
+    private void Awake()
+    { 
+        _grid = new Transform[Width, Height];
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        DrawEmptyCells();
+    }
+
+    void DrawEmptyCells()
+    {
+        if(EmptySprite)
+        {
+            for (int y = 0; y < Height - Header; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Transform _clone;
+                    _clone = Instantiate(EmptySprite, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                    _clone.name = "Board Space ( x = " + x.ToString() + " , y =" + y.ToString() + " )";
+                    _clone.transform.parent = transform;
+                }
+            }
+        }
     }
 }
