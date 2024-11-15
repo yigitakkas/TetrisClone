c4e940cddcbb9371cd37847b2a8f028440006f2b
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2024-11-15 16:19:45 +0300

    Create tetris shape prefabs and move methods

---
+++ Assets/Scripts/Core/Shape.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public bool CanRotate = true;
+
+    void Move(Vector3 moveDirection)
+    {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void RotateRight()
+    {
+        if(CanRotate)
+            transform.Rotate(0, 0, -90);
+    }
+    public void RotateLeft()
+    {
+        if (CanRotate)
+            transform.Rotate(0, 0, 90);
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
