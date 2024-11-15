4fa51a7dd1935fb53042171ebb0bf6aa58cee00c
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2024-11-15 22:33:45 +0300

    Implement input system with working move right

--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -1,17 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
     Board _gameBoard;
     Spawner _spawner;
     Shape _activeShape;
+    InputAction _test;
+    PlayerControls _inputActions;
+    bool pressing;
+    
 
-    float _dropInterval = .25f;
+    float _dropInterval = .15f;
     float _timeToDrop;
+    float _timeToNextKey;
+    [Range(0.02f,1f)]
+    public float KeyRepeatRate = .25f;
+
+    private void Awake()
+    {
+        _inputActions = new PlayerControls();
+        _test = _inputActions.Movement.MoveRight;
+        _inputActions.Movement.MoveRight.performed += MoveRight;
+        _inputActions.Movement.MoveRight.canceled += StopMovingRight;
+    }
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+    private void MoveRight(InputAction.CallbackContext ctx)
+    {
+        pressing = true;
+    }
+    private void StopMovingRight(InputAction.CallbackContext obj)
+    {
+        pressing = false;
+    }
+    public bool GetPressing()
+    {
+        return pressing;
+    }
     private void Start()
     {
+        _timeToNextKey = Time.time;
         _spawner = Spawner.Instance;
         _gameBoard = Board.Instance;
 
@@ -40,11 +79,20 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
-        if (!_gameBoard || !_spawner)
+        if (GetPressing() && Time.time > _timeToNextKey)
         {
-            return;
+            _activeShape.MoveRight();
+            _timeToNextKey = Time.time + KeyRepeatRate;
+            if (_gameBoard.IsValidPosition(_activeShape))
+            {
+                Debug.Log("Move right");
+            }
+            else
+            {
+                _activeShape.MoveLeft();
+                Debug.Log("hit the boundary");
+            }
         }
-
         if(Time.time > _timeToDrop)
         {
             _timeToDrop = Time.time + _dropInterval;
